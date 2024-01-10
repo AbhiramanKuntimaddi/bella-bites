04db81c3e279c11e6a38e7bfef2dd555df1e4a14
Abhiraman Kuntimaddi <abhiraman21696@icloud.com>
2024-01-10 13:10:54 +0100

    Fixed a shadow issue

--- app/page.tsx
+++ app/page.tsx
@@ -107,7 +107,7 @@ export default function Home(): JSX.Element {
 			<div className="mt-8">
 				<AnimatePresence>
 					<motion.div
-						className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 shadow-lg"
+						className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4"
 						initial={{ opacity: 0, y: 50 }}
 						animate={{ opacity: 1, y: 0 }}
 						transition={{ duration: 0.5 }}>
